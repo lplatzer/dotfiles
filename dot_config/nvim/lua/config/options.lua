bd7aabaf452c245354c2938a4604944a33b20bcf
Luis Platzer <luis@platzer.dev>
2026-06-04 20:47:37 +0200

    updated lua & run_once script

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Use basedpyright (not the default pyright) as the Python language server.
+-- ty (Astral) is added as a second server in lua/plugins/python.lua.
+vim.g.lazyvim_python_lsp = "basedpyright"
+
 vim.opt.winbar = "%=%m %f"
 
 vim.opt.tabstop = 4
